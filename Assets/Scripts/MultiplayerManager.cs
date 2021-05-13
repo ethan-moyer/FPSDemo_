edd11f44d482019e846a79cba6a1acbd468a72c1
Ethan <epestellmoyer@gmail.com>
2021-05-13 17:25:31 -0400

    Started work on hiding/showing layers for splitscreen.

--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -5,34 +5,12 @@ using UnityEngine.InputSystem;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private int maxPlayers;
-    [SerializeField] private Vector3 spawnPoint;
-    private List<PlayerInputReader> players;
+    [SerializeField] private LayerMask defaultMask;
+    private List<PlayerInput> players;
 
-    /*private void Awake()
+    public void OnPlayerJoined(PlayerInput input)
     {
-        if (playerPrefab.GetComponent<PlayerInputReader>() == null)
-        {
-            Debug.LogWarning("PlayerPrefab has no PlayerInputReader component.");
-            return;
-        }
-
-        for (int i = 0; i < maxPlayers; ++i)
-        {
-            PlayerInputReader newPlayer = Instantiate(playerPrefab, spawnPoint, Quaternion.identity).GetComponent<PlayerInputReader>();
-            if (i == 0)
-            {
-                newPlayer.AssignDevice(Keyboard.current);
-                newPlayer.AssignDevice(Mouse.current);
-            }
-            else
-            {
-                if (Gamepad.all.Count >= maxPlayers - 1)
-                {
-                    newPlayer.AssignDevice(Gamepad.all[i - 1]);
-                }
-            }
-        }
-    }*/
+        Camera cam = input.GetComponentInChildren<Camera>();
+        cam.cullingMask = defaultMask;
+    }
 }
