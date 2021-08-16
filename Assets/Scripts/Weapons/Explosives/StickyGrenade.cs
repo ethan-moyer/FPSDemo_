dbaed01e8e89e407a492b0f7fb0303bef9ee74ff
Ethan <epestellmoyer@gmail.com>
2021-08-15 21:48:48 -0400

    Fixed bug where stickygrenade would stay in the air after the player it was attached to dies.

--- Assets/Scripts/Weapons/Explosives/StickyGrenade.cs
+++ Assets/Scripts/Weapons/Explosives/StickyGrenade.cs
@@ -10,8 +10,24 @@ class StickyGrenade : Grenade
         if (collision.gameObject != player)
         {
             Rigidbody rb = GetComponent<Rigidbody>();
-            Destroy(rb);
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
             transform.SetParent(collision.transform, true);
+            if (collision.gameObject.layer == 9)
+            {
+                PlayerController pc = collision.gameObject.GetComponent<PlayerController>();
+                if (pc != null)
+                {
+                    pc.Die.AddListener(OnPlayerDie);
+                }
+            }
         }
+    }
+
+    private void OnPlayerDie(int playerID, int otherPlayerID)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
     }
 }
